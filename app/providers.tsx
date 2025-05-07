30d45742033b5690283f41f3d0d6434bbddcfc3e
Gabriel Elias <gabrielelias8@hotmail.com>
2025-05-07 13:23:27 -0300

    Added PostHog Proxy

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,7 +10,7 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react'
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://daily-programming-concept.vercel.app/ingest',
       person_profiles: 'identified_only',
       capture_pageview: false,
     })
--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ]
+  }
 }
 
 export default nextConfig
